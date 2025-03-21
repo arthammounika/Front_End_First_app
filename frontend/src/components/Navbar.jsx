6e8f8c4b19992f3d6932ff32df1de22bef1fe27c
dameruppula-nandini <nandinidameruppula16@gmail.com>
2025-03-21 19:32:13 +0530

    firstappfrontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { StoreContext } from '../context/StoreContext';
+import { assets } from './../assets/assets';
+import './Navbar/Navbar.css';
+
+
+
+const Navbar = ({setShowLogin}) => {
+    const [menu,setMenu]=useState("home");
+    const {getTotalCartAmount,token,setToken}=useContext(StoreContext);
+  return (
+    <div className='navbar'>
+       <Link to='/' ><img src={assets.logo} alt="" className="log" /></Link>
+       <ul className="navbar-menu">
+        <Link to="/" onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+        <a href= "#explore-menu" onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
+        <a href="#app-download" onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
+        <a href="#footer" onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
+       </ul>
+       <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <Link to='/cart' className='navbar-search-icon'>
+          <img src={assets.basket_icon} alt="" />
+          <div className={getTotalCartAmount() > 0 ? "dot" : ""}></div>
+        </Link>
+        {!token ? <button onClick={() => setShowLogin(true)}>sign in</button>
+          : <div className='navbar-profile'>
+            <img src={assets.profile_icon} alt="" />
+            <ul className='navbar-profile-dropdown'>
+              <li onClick={()=>navigate('/myorders')}> <img src={assets.bag_icon} alt="" /> <p>Orders</p></li>
+              <hr />
+              <li onClick={logout}> <img src={assets.logout_icon} alt="" /> <p>Logout</p></li> 
+            </ul>
+          </div>
+        }
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
